b93a2b8075ab92d4e728fc4984a2ed35f5bcdbc4
nduruoz <duruozmal@berkeley.edu>
2026-07-18 01:09:12 +0300

    Remove all debug code

--- notifier.py
+++ notifier.py
@@ -4,23 +4,14 @@ import requests
 TOPIC = os.getenv("NTFY_TOPIC")
 
 def notify(title, message):
-    headers = {
-        "Title": title,
-        "Priority": "5",
-        "Tags": "warning,books",
-    }
-
-    print("TITLE:", repr(title), flush=True)
-    print("TOPIC:", repr(TOPIC), flush=True)
-    print("HEADERS:", repr(headers), flush=True)
-    print("Before requests.post", flush=True)
-
     response = requests.post(
         f"https://ntfy.sh/{TOPIC}",
         data=message.encode("utf-8"),
-        headers=headers,
+        headers={
+            "Title": title,
+            "Priority": "5",
+            "Tags": "warning,books",
+        },
     )
-
-    print("After requests.post", flush=True)
 
     response.raise_for_status()
